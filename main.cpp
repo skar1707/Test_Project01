cf97e1526892139fe7afbeaaae402b5618547176
Shubham Kumar Agarwal <shubhamkumaragarwal8@gmail.com>
2021-05-13 22:48:41 +0530

    init

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//#include <iostrem>
+//#include <stdio.h>
+
+// int main(int argc, char const *argv[])
+// {
+//     std::cout<<"Hello Docker Container!"<<std::endl;
+//     return 0;
+// }
+
+#include <stdio.h>
+int main() {
+   // printf() displays the string inside quotation
+   printf("Hello, World!");
+   return 0;
+}
